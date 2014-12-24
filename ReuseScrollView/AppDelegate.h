c92c6b7ec4bbac057f14d25f283435e0254cd4f1
Jason Zhou 周正昌 <zzhou006@ucr.edu>
2014-12-24 13:49:47 +0800

    Initial Commit

---
+++ ReuseScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReuseScrollView
+//
+//  Created by Jason on 12/24/14.
+//  Copyright (c) 2014 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReuseScrollViewTests/ReuseScrollViewTests.m
@@ -0,0 +1,40 @@
+//
+//  ReuseScrollViewTests.m
+//  ReuseScrollViewTests
+//
+//  Created by Jason on 12/24/14.
+//  Copyright (c) 2014 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ReuseScrollViewTests : XCTestCase
+
+@end
+
+@implementation ReuseScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
